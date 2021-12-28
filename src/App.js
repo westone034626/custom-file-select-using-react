b1b0ecc6a6a184234946f0e461adbcce89229676
seows <seows@laivdata.com>
2021-12-28 17:27:07 +0900

    React로 custom file input 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div>hi</div>;
+  const [selectedFileName, setSelectedFileName] = useState();
+  const onInputChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    const path = value.split('\\');
+    const fileName = path[path.length - 1];
+    setSelectedFileName(fileName);
+  };
+
+  return (
+    <form>
+      <label htmlFor="upload">
+        <span>{selectedFileName || 'Choose File'}</span>
+      </label>
+      <input
+        onChange={onInputChange}
+        style={{ display: 'none' }}
+        type="file"
+        id="upload"
+      />
+    </form>
+  );
 }
 
 export default App;

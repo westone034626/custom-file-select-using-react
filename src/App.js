a5d01f800514ef9e7a49015edc03442e06f2eb64
seows <seows@laivdata.com>
2021-12-28 17:59:09 +0900

    파일 취소 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
   const [selectedFileName, setSelectedFileName] = useState();
+  const inputRef = useRef();
+
   const onInputChange = (e) => {
     const {
       target: { value },
@@ -12,17 +14,26 @@ function App() {
     setSelectedFileName(fileName);
   };
 
+  const onClickCancel = (e) => {
+    e.preventDefault();
+    if (!inputRef.current) return;
+    inputRef.current.value = '';
+    setSelectedFileName('');
+  };
+
   return (
     <form>
       <label htmlFor="upload">
         <span>{selectedFileName || 'Choose File'}</span>
       </label>
       <input
+        ref={inputRef}
         onChange={onInputChange}
         style={{ display: 'none' }}
         type="file"
         id="upload"
       />
+      {selectedFileName && <button onClick={onClickCancel}>x</button>}
     </form>
   );
 }
